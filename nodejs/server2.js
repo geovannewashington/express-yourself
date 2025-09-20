9a06b77b347d7a379911db62f499aa0d8a1f5e3e
Geovanne Washington <geoxp98@gmail.com>
2025-09-20 11:28:30 -0300

    Pushing data to the fake database

--- nodejs/server2.js
+++ nodejs/server2.js
@@ -43,7 +43,25 @@ const getUserByIdHandler = (req, res) => {
     res.end(JSON.stringify(user) + '\n');
 };
 
-// 3. Not found handler 
+// 3. Route handler for POST /api/users -> register a new user into the fake DB (users array)
+const createUserHandler = (req, res) => {
+    let body = '';
+    // Listen for data 
+    req.on('data', (chunk) => body += chunk.toString());
+    
+    
+    req.on('end', () => {
+        // If no data was passed
+        if (!body) return notFoundHandler(req, res, 'No data posted'); 
+        
+        const newUser = JSON.parse(body); // converting from JSON to normal JS object
+        users.push(newUser);
+        res.statusCode = 201; // 201 basically means success + something has changed
+        res.end(JSON.stringify(newUser) + '\n'); // So it logs the user we just added
+    });
+};
+
+// 4. Not found handler 
 const notFoundHandler = (req, res, msg = 'Route not found') => {
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: msg }) + '\n');
@@ -72,6 +90,11 @@ use((req, res) => {
         return getUsersHandler(req, res);
     }
     
+    // POST new user into the fata DB (users array)
+    if (req.url === '/api/users' && req.method === 'POST') {
+        return createUserHandler(req, res);
+    }
+
     // Query for a specific user by id    
     if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
         return getUserByIdHandler(req, res);
