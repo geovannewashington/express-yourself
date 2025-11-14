11b792dbf5896a5c1a4209baf3b36e466a2b7c7a
Geovanne Washington <geoxp98@gmail.com>
2025-11-14 10:24:23 -0300

    add: making a post request to create a new post

--- expressjs/routes/posts.js
+++ expressjs/routes/posts.js
@@ -46,11 +46,19 @@ router.get('/:id', (req, res) => {
 
 // Create a new post
 router.post('/', (req, res) => {
-    console.log(req.body); 
-    // Note that if we don't use specific request parsers in the server.js file (use the middlewares)
-    // this will log undefined
+    const postTitle = req.body.title;
+    
+    if (!postTitle) {
+        return res.status(400).json({"msg": "Please include a title"});
+        // code 400 is usually a client error, and error with the data the client sent
+    }
+    const newPost = {
+       id : posts.length + 1, // On a real database the id would be set automatically
+       title: postTitle
+    };
+
+    posts.push(newPost);
     res.status(201).json(posts);
-    // code 201 means the request was successfull AND something was created.
 });
 
 export default router;
